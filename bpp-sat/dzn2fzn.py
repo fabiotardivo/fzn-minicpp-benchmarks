4fb16115f7561d5de26e70639b82f467344c2d33
Fabio Tardivo <tardivo.fabio@gmail.com>
2025-09-27 15:12:05 -0400

    Fresh start

---
+++ bpp-sat/dzn2fzn.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import subprocess
+
+dzn_dir   = "dzn"
+fzn_dir   = "fzn-sat"
+solver_id = "org.minicpp.ml"
+model_filename = "bpp-sat.mzn"
+silution_filename = "solutions.csv"
+
+solutions = pd.read_csv(silution_filename)
+for _, row  in solutions.iterrows():
+    dzn_filename, lb, ub = row
+    dzn_filepath = f"{dzn_dir}/{dzn_filename}"
+    print(f"Converting {dzn_filepath} ", end="")
+    if (lb == ub):
+        fzn_filename = dzn_filename.replace(".dzn", ".fzn")
+        fzn_filepath = f"{fzn_dir}/{fzn_filename}"
+        result = subprocess.run([
+            "minizinc",
+            "--solver", solver_id,
+            "--compile", "--no-output-ozn",
+            model_filename,
+            dzn_filepath, "-D", f"n_bins={ub}",
+            "-o", fzn_filepath])
+        print("[OK]" if result.returncode == 0 else "[FAILED]")
+    else:
+        print("[SKIPPED]")
